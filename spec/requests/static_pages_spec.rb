9cee2697ce9f473be158a25d9e2dd3806cde41cb
manuel79 <manuel79@gmx.at>
2013-11-21 21:54:28 +0100

    Create user controller and signup page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,14 +2,13 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
 
   describe "Home page" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title(base_title) }
+    it { should have_title(BASE_TITLE) }
     it { should_not have_title('| Home') }
   end
 
@@ -17,20 +16,20 @@ describe "Static pages" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    it { should have_title("#{base_title} | Help") }
+    it { should have_title("#{BASE_TITLE} | Help") }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_content('About Us') }
-    it { should have_title("#{base_title} | About Us") }
+    it { should have_title("#{BASE_TITLE} | About Us") }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    it { should have_title("#{base_title} | Contact") }
+    it { should have_title("#{BASE_TITLE} | Contact") }
   end
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,1 @@
+BASE_TITLE = "Ruby on Rails Tutorial Sample App"
